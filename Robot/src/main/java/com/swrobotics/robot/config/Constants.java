88bdc3a04d6c27884638339d28c7936f55e3aba2
rmheuer <63077980+rmheuer@users.noreply.github.com>
2025-02-13 07:30:47 -0600

    Update outtake pivot gear ratio for new pulley

--- Robot/src/main/java/com/swrobotics/robot/config/Constants.java
+++ Robot/src/main/java/com/swrobotics/robot/config/Constants.java
@@ -174,8 +174,8 @@ public final class Constants {
 
     // Coral outtake pivot
     // 60:24 CANcoder
-    public static final double kOuttakePivotMotorToArmRatio = (60.0 / 24.0) * (58.0 / 18.0) * (60.0 / 8.0);
-    public static final double kOuttakePivotCANcoderToArmRatio = 60.0 / 24.0;
+    public static final double kOuttakePivotMotorToArmRatio = (60.0 / 27.0) * (58.0 / 18.0) * (60.0 / 8.0);
+    public static final double kOuttakePivotCANcoderToArmRatio = 60.0 / 27.0;
     public static final NTEntry<Double> kOuttakePivotEncoderOffset = new NTDouble("Superstructure/Pivot/Encoder/Offset (rot)", 0).setPersistent();
     public static final NTSlot0Configs kOuttakePivotPID =
             new NTSlot0Configs("Superstructure/Pivot/PID", 200, 0, 0.316, 0, 7.021702, 0);
